0431147ec69d02a699019ee7c4c40e4a207be28a
Pacharapol Withayasakpunt <polv@polv.cc>
2020-06-02 12:19:40 +0700

    fix search-engine

--- components/PostQuery.vue
+++ components/PostQuery.vue
@@ -4,13 +4,13 @@ section
     h2.title.is-2
       span(style="margin-right: 0.5em;") Tag:
       span(style="font-weight: 400") {{tag}}
-  section(v-if="posts && posts.length > 0")
+  b-loading(v-if="isLoading")
+  section(v-else-if="posts.length > 0")
     PostTeaser(v-for="p in posts" :post="p" :key="p.name")
     b-pagination.my-lg.mx-sm(:current.sync="page" :total="count" :per-page="5"
     icon-prev="caret-left" icon-next="caret-right" size="default" rounded)
-  div(v-else-if="posts")
-    empty
   div(v-else)
+    empty
 </template>
 
 <script lang="ts">
@@ -32,7 +32,8 @@ export default class PostQuery extends Vue {
   }
 
   count = 0
-  posts: any[] | null = null
+  posts: any[] = []
+  isLoading = true
 
   get q() {
     return normalizeArray(this.$route.query.q) || ''
@@ -45,7 +46,10 @@ export default class PostQuery extends Vue {
   set page(p) {
     const { path } = this.$route
     const path0 = path.replace(/\/(\d+)?$/, '')
-    this.$router.push(`${path0 || '/blog'}${p === 1 ? '' : `/${p}`}`)
+    this.$router.push({
+      path: `${path0 || '/blog'}${p === 1 ? '' : `/${p}`}`,
+      query: this.$route.query
+    })
   }
 
   get tag() {
@@ -59,6 +63,7 @@ export default class PostQuery extends Vue {
   @Watch('page')
   @Watch('tag')
   async updatePosts() {
+    this.isLoading = true
     if (this.q) {
       const ps = await this.$axios.$post(
         '/.netlify/functions/search',
@@ -78,16 +83,16 @@ export default class PostQuery extends Vue {
       this.count = this.defaults.count
       this.$set(this, 'posts', this.defaults.posts)
     }
+    this.isLoading = false
   }
 
   @Watch('q')
   onQChanged() {
     this.$router.push({
       path: '/blog',
-      query: {
-        q: this.q
-      }
+      query: this.$route.query
     })
+    this.page = 1
     this.updatePosts()
   }
 }
