8c80fd112155272b41d1dde635984e2f38d51831
Pacharapol Withayasakpunt <polv@polv.cc>
2020-06-20 15:19:18 +0700

    🔨 Refactor: remove credentials from nuxt.config.ts
    specifically theme.title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,14 +15,14 @@ export default async () => {
       htmlAttrs: {
         lang: 'en'
       },
-      title: "polv's homepage",
+      title: theme.title,
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
           name: 'description',
-          content: "polv's homepage"
+          content: theme.description || ''
         }
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
